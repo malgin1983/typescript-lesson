dc22cd1ca17954bca1a107e20ffa2d888685e917
VADIM MALGIN <45521129+malgin1983@users.noreply.github.com>
2021-11-28 18:38:03 +0000

    Create typescript-lesson.ts

---
+++ typescript-lesson.ts
@@ -0,0 +1,48 @@
+
+//Partial переводит оюязательные в не обязательные
+type A = {one: string; two: number}
+const ob1: A = {
+    one: 'One',
+    two: 34,
+}
+
+type B = Partial<A>
+const ob2: B ={
+    one: 'Two'
+}
+
+ type C<T> =  {[K in keyof T]?: T[K]}
+ const ob3: C<A> = {
+    two: 4
+ }
+
+ //Requare переводит необязательные поля в обязательные
+ type NR = { one?: number;  two?: string; three: boolean }
+ type RR = Required<NR>
+ type RRR<T> = {[K in keyof  T]-?: T[K]}
+ const ob4: RRR<NR> = {one: 1, two: 'aaa', three: false}
+
+
+//Pick дает возмлжность выбрать свойста из тинн
+type F = { one: string, two: number; three: boolean}
+
+type P = Pick<F, 'two' | 'one'>
+type PP<A, B extends  keyof  A> = {[C in B]: A[C]}
+
+const newObject: P =  {
+    one: 'bbbb',
+    two: 32,
+}
+
+const newObj2: PP<F, 'one' | 'two'> = {
+    one: 'aaaa',
+    two: 32,
+}
+
+//Omit дает  возможность исключить свойство
+
+ type D = Omit<F, 'title'>
+ type myOmit<T, K> = Pick<T, Exclude<keyof T, K>>
+ 
+const ob5: myOmit<F, 'two'> = { one: 'aaaa', three: false}
+ 
