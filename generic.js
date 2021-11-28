ad104f8cdc7fee5ed28b88e53e3b172ad0738b80
malgin1983 <malghin@yandex.ru>
2021-11-28 21:57:18 +0300

    commit

---
+++ generic.js
@@ -0,0 +1,16 @@
+var ob1 = {
+    one: 'One',
+    two: 34
+};
+var ob2 = {
+    one: 'Two'
+};
+var ob3 = {
+    two: 4
+};
+var ob4 = { one: 1, two: 'aaa', three: false };
+var ob5 = {
+    description: 'string;',
+    completed: false,
+    createdAt: 32
+};
